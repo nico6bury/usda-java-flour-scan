4d0ed3b53770092eece878c16913b97f6403a8f0
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-06-23 09:33:05 -0500

    Reformatted result types since Result is now a generic class.

--- src/Scan/Scan.java
+++ src/Scan/Scan.java
@@ -21,50 +21,50 @@ public class Scan {
      * Tries to find a valid TwainSource and save it for later.
      * @return Returns an error if one was thrown, otherwise is Ok.
      */
-    public Result initScanner() {
+    public Result<Result.ResultType> initScanner() {
         // make an attempt to initialize the scanner
         try {
             // TODO: Figure out which source to use
         }//end trying to figure out the twain source
         catch (Exception e) {
-            return new Result(e);
+            return new Result<>(e);
         }//end catching any exceptions
 
         // if we've reached this point, we must be fine
-        return new Result();
+        return new Result<>();
     }//end initScanner()
 
     /**
      * Tries to set the scan settings of the saved twain source.
      * @return Returns an error if one was thrown, otherwise is Ok.
      */
-    public Result setScanSettings() {
+    public Result<Result.ResultType> setScanSettings() {
         // make an attempt to set settings of twain source
         try {
             // TODO: Set the settings for the scanner
         }//end trying to set scan settings
         catch (Exception e) {
-            return new Result(e);
+            return new Result<>(e);
         }//end catching any exceptions
 
         // if we've reached this point, we must be fine
-        return new Result();
+        return new Result<>();
     }//end setScanSettings()
 
     /**
      * Tries to run the scanner of the saved twain source with proper settings.
      * @return Returns an error if one was thrown, otherwise is Ok.
      */
-    public Result runScanner() {
+    public Result<String> runScanner() {
         // make an attempt to run the scanner
         try {
             // TODO: run the scanner
         }//end trying to run the scanner
         catch (Exception e) {
-            return new Result(e);
+            return new Result<>(e);
         }//end catching any exceptions
 
         // if we've reached this point, we must be fine
-        return new Result();
+        return new Result<>();
     }//end runScanner()
 }//end class Scan
