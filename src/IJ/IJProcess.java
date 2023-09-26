0a73ac5f59b9960755e6cdbdd58ccc6e937df123
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-09-26 11:02:50 -0500

    Added IJProcess skeleton class.

---
+++ src/IJ/IJProcess.java
@@ -0,0 +1,10 @@
+package IJ;
+
+/**
+ * This class keeps track of everything related to running files through the imagej flour macros.
+ * It also handles all the calls to imagej.
+ * @author Nicholas Sixbury
+ */
+public class IJProcess {
+    
+}//end class IJProcess
