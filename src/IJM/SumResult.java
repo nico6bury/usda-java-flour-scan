c86a3b8a22db72e8e1ec60683ce3f635646ef4bd
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-10-27 13:25:22 -0500

    Added SumResult, to take the place of the summary window in imagej.

---
+++ src/IJM/SumResult.java
@@ -0,0 +1,15 @@
+package IJM;
+
+public class SumResult {
+    public int slice;
+    public int count;
+    public int total_area;
+    public double percent_area;
+
+    public SumResult(int slice, int count, int total_area, double percent_area) {
+        this.slice = slice;
+        this.count = count;
+        this.total_area = total_area;
+        this.percent_area = percent_area;
+    }
+}
