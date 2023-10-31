d5f2a6fd9887151fbaccac3089c6850201487172
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-10-31 12:32:57 -0500

    Added L stats to the SumResult

--- src/IJM/SumResult.java
+++ src/IJM/SumResult.java
@@ -5,11 +5,24 @@ public class SumResult {
     public int count;
     public int total_area;
     public double percent_area;
+    public double l_mean;
+    public double l_stdv;
+
+    public SumResult() {}
 
     public SumResult(String slice, int count, int total_area, double percent_area) {
         this.slice = slice;
         this.count = count;
         this.total_area = total_area;
         this.percent_area = percent_area;
-    }
+    }//end constructor
+
+    public SumResult(String slice, int count, int total_area, double percent_area, double l_mean, double l_stdv) {
+        this.slice = slice;
+        this.count = count;
+        this.total_area = total_area;
+        this.percent_area = percent_area;
+        this.l_mean = l_mean;
+        this.l_stdv = l_stdv;
+    }//end constructor
 }
