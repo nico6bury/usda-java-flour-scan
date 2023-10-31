9d5de1f6df64f04d9c1e86c75b908b183de83e97
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2023-10-31 12:44:57 -0500

    Added some folder name related constants.

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -14,4 +14,6 @@ public class Constants {
     public static String PEOPLE = "Sixbury/Rust/Brabec";
     public static String PROGRAM_NAME = "Flour Scan & Analysis";
     public static String VERSION = "v1.0.0";
+    public static String SCANNED_IMAGES_FOLDER_NAME = "scanned-images";
+    public static String IMAGEJ_OUTPUT_FOLDER_NAME = "output-folder";
 }//end class Constants
